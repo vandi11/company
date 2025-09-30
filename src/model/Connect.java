a319a0a3e63495bc25ed023ea990ba578d61ce3c
Andi <vinczepalkoandrea@ktch.hu>
2025-09-30 19:25:21 +0200

    try to push

---
+++ src/model/Connect.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLDataException;
+
+/**
+ *
+ * @author Diak
+ */
+public class Connect {
+    
+    private Connection conn;
+    private String dbName;
+    private String user;
+    private String pass;
+    
+    public Connect() {
+        
+    }
+}
