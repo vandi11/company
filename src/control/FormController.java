677102b67a785fcdf9199853d6631f0aaaadd2d6
Andi <vinczepalkoandrea@ktch.hu>
2025-09-30 19:08:22 +0200

    database connection project

---
+++ src/control/FormController.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package control;
+
+import view.MainForm;
+
+/**
+ *
+ * @author Diak
+ */
+
+public class FormController {
+    
+    private MainForm mainFrm;
+    
+    public FormController() {
+        
+        mainFrm = new MainForm();
+        mainFrm.setVisible( true);
+        
+    }
+    
+}
